41bd57ee5ccb912293be933d650123b706c2e8a3
Naoto Toyama <m9m6n1m8t51080@gmail.com>
2021-02-18 20:34:59 +0900

    弾を発射

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
-import pygame
+import pygame as pg
 from pygame.locals import *
 import sys
 import math
 
 SCREEN = Rect(0,0,400,400)
+SCREEN_COLOR = "#FFFFE0"
 
-class Player(pygame.sprite.Sprite):
+class Player(pg.sprite.Sprite):
   def __init__(self, pos, v, r, w):
-    pygame.sprite.Sprite.__init__(self)
-    self.image_data = pygame.transform.smoothscale(pygame.image.load("player.png").convert_alpha(), (2*r,2*r))
+    pg.sprite.Sprite.__init__(self)
+    self.image_data = pg.transform.smoothscale(pg.image.load("player.png").convert_alpha(), (2*r,2*r))
     self.r = r
     self.image = self.image_data
     self.rect = self.image.get_rect()
@@ -28,20 +29,19 @@ class Player(pygame.sprite.Sprite):
     if self.rect.centery+self.r>SCREEN.size[1]:
       self.rect.centery = SCREEN.size[1]-self.r
     # 向き
-    mouseV = pygame.math.Vector2(pygame.mouse.get_pos())
-    posV = pygame.math.Vector2(self.rect.center)
-    angleV = pygame.math.Vector2((math.sin(math.radians(self.theta))*-1, math.cos(math.radians(self.theta))*-1))
+    mouseV = pg.math.Vector2(pg.mouse.get_pos())
+    posV = pg.math.Vector2(self.rect.center)
+    angleV = pg.math.Vector2((math.sin(math.radians(self.theta))*-1, math.cos(math.radians(self.theta))*-1))
     dtheta = (mouseV-posV).angle_to(angleV)
-    print(dtheta)
     if (-180<dtheta and dtheta<0-self.w) or (180<dtheta and dtheta<360-self.w):
       self.theta -= self.w
     elif (-360+self.w<dtheta and dtheta<-180) or (self.w<dtheta and dtheta<180):
       self.theta += self.w
     center = self.rect.center
-    self.image = pygame.transform.rotate(self.image_data, self.theta)
+    self.image = pg.transform.rotate(self.image_data, self.theta)
     self.rect = self.image.get_rect()
     self.rect.center = center
-
+  
   def move(self, key):
     if key == K_UP:
       self.rect.centery -= self.v[1]
@@ -51,36 +51,59 @@ class Player(pygame.sprite.Sprite):
       self.rect.centerx -= self.v[0]
     elif key == K_RIGHT:
       self.rect.centerx += self.v[0]
+  
+  def shoot(self):
+    self.groups()[0].add(Bullet(self.rect.center, 5, (5,10), self.theta))
     
+class Bullet(pg.sprite.Sprite):
+  def __init__(self, pos, v, size, theta):
+    pg.sprite.Sprite.__init__(self)
+    self.image_data = pg.transform.smoothscale(pg.image.load("bullet.png").convert_alpha(), size)
+    self.image = pg.transform.rotate(self.image_data, theta)
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+    self.v = v
+    self.theta = theta
+    self.time = 0
+  def update(self):
+    self.time += 1
+    if self.time>30:
+      self.kill()
+    self.move()
+  def move(self):
+    angleV = pg.math.Vector2((math.sin(math.radians(self.theta))*-1, math.cos(math.radians(self.theta))*-1))
+    self.rect.center = (pg.math.Vector2(self.rect.center) + self.v * angleV)[:]
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode(SCREEN.size)
-  pygame.display.set_caption("foo")
+  pg.init()
+  screen = pg.display.set_mode(SCREEN.size)
+  pg.display.set_caption("foo")
+  group = pg.sprite.Group()
   player = Player(SCREEN.center, [5,5], 30, 5)
-  group = pygame.sprite.Group()
   group.add(player)
+  
+  pg.key.set_repeat(30, 30)
 
-  pygame.key.set_repeat(30, 30)
-
-  clock = pygame.time.Clock()
+  clock = pg.time.Clock()
   while True:
     clock.tick(30)
-    screen.fill((155,155,155,0))
+    screen.fill(SCREEN_COLOR)
     group.update()
     group.draw(screen)
-    pygame.display.flip()
+    pg.display.flip()
 
-    for event in pygame.event.get():
+    for event in pg.event.get():
       if event.type == QUIT:
-        pygame.quit()
+        pg.quit()
         sys.exit()
       if event.type == KEYDOWN:
         if event.key == K_ESCAPE:
-          pygame.quit()
+          pg.quit()
           sys.exit()
-        else:
+        elif event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
           player.move(event.key)
+        elif event.key == K_SPACE:
+          player.shoot()
 
 if __name__ == "__main__":
   main()
